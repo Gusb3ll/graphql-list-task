49d27e32c2d4e4a92b8cfe82575d4552ec18ed6b
Kitpipat Jaritwong <77166960+Gusb3ll@users.noreply.github.com>
2022-06-01 20:49:13 +0700

    docs: add document 📓

--- src/resolvers/list.resolver.ts
+++ src/resolvers/list.resolver.ts
@@ -13,7 +13,7 @@ import listService from '../services/list.service'
 
 @Resolver()
 export class listResolver {
-  @Query(() => getAllListResponse)
+  @Query(() => getAllListResponse, { description: 'Get all list' })
   async getAllList(): Promise<getAllListResponse> {
     try {
       const data = await listService.getAllList()
@@ -24,7 +24,7 @@ export class listResolver {
     }
   }
 
-  @Query(() => getListByIdResponse)
+  @Query(() => getListByIdResponse, { description: 'Get list by id' })
   async getListById(@Arg('listId', () => Number) listId: number): Promise<getListByIdResponse> {
     try {
       const data = await listService.getListById(listId)
@@ -35,7 +35,7 @@ export class listResolver {
     }
   }
 
-  @Mutation(() => createListResponse)
+  @Mutation(() => createListResponse, { description: 'Create a list' })
   async createList(@Arg('title', () => String) title: string, @Arg('description', () => String) description?: string): Promise<createListResponse> {
     try {
       const data = await listService.createList(title, description)
@@ -49,7 +49,7 @@ export class listResolver {
     }
   }
 
-  @Mutation(() => createTaskResponse)
+  @Mutation(() => createTaskResponse, { description: 'Create a task' })
   async createTask(@Arg('listId', () => Number) listId: number, @Arg('title', () => String) title: string, @Arg('description', () => String) description?: string): Promise<createTaskResponse> {
     try {
       const data = await listService.createTask(listId, title, description)
@@ -63,7 +63,7 @@ export class listResolver {
     }
   }
 
-  @Mutation(() => updateTaskResponse)
+  @Mutation(() => updateTaskResponse, { description: 'Update task by id' })
   async updateTask(@Arg('taskId', () => Number) taskId: number, @Arg('title', () => String) title?: string, @Arg('description', () => String) description?: string, @Arg('completed', () => Boolean) completed?: boolean): Promise<updateTaskResponse> {
     try {
       const data = await listService.updateTask(taskId, title, description, completed)
@@ -77,7 +77,7 @@ export class listResolver {
     }
   }
 
-  @Mutation(() => moveTaskResponse)
+  @Mutation(() => moveTaskResponse, { description: 'Move task using both tasks id\'s' })
   async moveTask(@Arg('fromId', () => Number) fromId: number, @Arg('toId', () => Number) toId: number): Promise<moveTaskResponse> {
     try {
       await listService.moveTask(fromId, toId)
